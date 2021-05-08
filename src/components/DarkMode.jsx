25b7758657e8f5a2a57af356c573dc6d1ed2b9a0
James Rosas <jamesadrian777@gmail.com>
2021-05-08 18:10:59 -0500

    Fisrt commit, making DarkMode example  with useState

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './styles/DarkMode.css'
+
+function DarkMode() {
+    const [mode, setMode] = useState(false)
+
+    const handleClick = () =>{
+        setMode(!mode)
+    }
+
+    const container = document.querySelector('.mode-container');
+    if (mode === true){
+        container.classList.add('dark-background')
+    }
+    if (mode === false){
+        container.classList.remove('dark-background')
+    }
+
+    return(
+        <div className="mode-container">
+            <p>Change Mode</p>
+            <button onClick={handleClick}>{mode ? 'Ligth Mode': 'Dark Mode'}</button>
+        </div>
+    )
+}
+
+export default DarkMode;
