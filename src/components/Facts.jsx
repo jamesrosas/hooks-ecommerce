af10ff376aa3b9102e00e0c36e635490942a5419
James Rosas <jamesadrian777@gmail.com>
2021-05-12 11:02:22 -0500

    utilice useReducer para agregar personajes favoritos por medio de un boton

--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -1,9 +1,28 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import Target from './Target';
 import './styles/Facts.css';
 
+const initialState = {
+    favorites: []
+}
+
+const favoriteReducer = (state, action) => {
+    switch(action.type){
+        case 'ADD_FAVORITES':
+            return {
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            }
+        default:
+            return state
+            
+    }
+}
+
+
 function Facts(props){
     const [datos , setData] = useState([]);
+    const [favorites, dispatch] = useReducer(favoriteReducer, initialState)
 
     const getData = async() => {
         const respuesta = await fetch('https://rickandmortyapi.com/api/character');
@@ -15,15 +34,37 @@ function Facts(props){
         getData(); 
     }, []);
 
+    const handleClick = favorite =>{
+        dispatch({
+            type: 'ADD_FAVORITES',
+            payload: favorite
+        })
+    }
+
+
     return(
-        <div className={props.className}>
-            {datos.map((person)=>{
-                return(
-                    <Target image={person.image} name={person.name} planet={person.location.name}/>
+        <React.Fragment>
+            <div className="favorites-container">
+                <h3>Favorites</h3><br/>
+                {favorites.favorites.map(favorite=>(
+                    <li key={favorite.id}>
+                        <img className="favorite-badge" src={favorite.image}/>
+                    </li>
+                ))}
+            </div>
+            <div className={props.className}>
+                {datos.map((person)=>{
+                    return(
+                        <div key={person.id}>
+                            <Target image={person.image} name={person.name} planet={person.location.name}/>
+                            <button className="favorite-button" onClick={() => handleClick(person)} >Agregar a Favoritos</button> 
+                        </div>
+                    )}
                 )}
-            )}
-        </div>
-        )
+            </div>
+        
+        </React.Fragment>
+    )
 }
 
 export default Facts;
