72b5a58946c2c3e1c96998337fe2f4973fb92121
James Rosas <jamesadrian777@gmail.com>
2021-05-18 11:33:38 -0500

    mejoramos al UI de los favoritos, añadimos boton de eliminar--> osea añdimos en el switch del reducer un case para eliminar el favorito, y asi mismo modificamos el case de agregar para que no deje agregar el mismo personaje dos veces

--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -10,9 +10,23 @@ const initialState = {
 const favoriteReducer = (state, action) => {
     switch (action.type) {
         case 'ADD_FAVORITES':
+            const isExist = state.favorites.find(item =>
+                item === action.payload)
+            if(isExist) {
+                return {
+                    ...state,
+                }
+            }
             return {
                 ...state,
                 favorites: [...state.favorites, action.payload] //payload es como la informacion nueva que se recibira en el state, esta es una informacion que se recibe en forma de objeto. el action payload no equivale a toda la tarea que se retorna de nuestro type, sino que es solo donde usamos action.payload que es la informacion nueva que se agrega en state. entonces con ...state copiamos todo lo que hay en el state global, y con favorite: [...state.favorites, ] todo lo que ya hay en nuestra key de favorites. de modo que despues cuanod queramos acceder a esta informarcion usaremos la ruta  state.favorites  , ya que recordemos que la palabra state fue la que definimos e nuestro hook de useReducer como nuestro estado global por esto tenemos que const [state, dispatch] = useReducer(favoriteReducer, initialState)  entnces como vemos initialState es nuestro mismo state solo que con el nombre de la constante donde tenemos guardado nuestro estado.
+            }
+        case 'REMOVE_FAV':
+            return {
+                ...state,
+                favorites: [...state.favorites.filter(favs => 
+                    favs !== action.payload
+                )]
             }
         default:
             return state
@@ -48,6 +62,13 @@ function Facts(props) {
         })
     }
 
+    const handleClickRemove = (favorite) => {
+        dispatch({
+            type: 'REMOVE_FAV',
+            payload: favorite
+        })
+    }
+
     const filteredPersons = useMemo(() => datos.filter((data) =>{
         return data.name.toLowerCase().includes(searchState.toLowerCase())
     })
@@ -72,9 +93,10 @@ function Facts(props) {
             <div className="favorites-container">
                 <h3>Favorites</h3><br />
                 {myState.favorites.map(favorite => (
-                    <li key={favorite.id}>
+                    <li  className="li-favorites" key={favorite.id}>
                         {/* <p>{favorite.img} {favorite.name}</p> */}
                         <img className="favorite-badge" src={favorite.image}/>
+                        <button className="btn-delete_fav" onClick={()=> handleClickRemove(favorite)}>x</button>
                     </li>
                 ))}
             </div>
@@ -83,7 +105,7 @@ function Facts(props) {
                     return (
                         <div key={person.id}>
                             <Target image={person.image} name={person.name} planet={person.location.name} />
-                            <button className="favorite-button" onClick={() => handleClick(person)} >Agregar a Favoritos</button>
+                            <button className="favorite-button"  onClick={() => handleClick(person)} >Agregar a Favoritos</button>
                         </div>
                     )
                 }
